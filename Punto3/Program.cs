4634d8dae4bee7355b8fa10c64b14500b3cefab1
Mayra <mayri_luli@hotmail.com>
2026-06-09 16:50:02 -0300

    WIP texto

---
+++ Punto3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un texto: ");
+String Texto1 = Console.ReadLine();
+Console.WriteLine($"""
+    La longitud de {Texto1} es de: {Texto1.Length}
+""");
+Console.WriteLine("Ingrese otro texto: ");
+String Texto2 = Console.ReadLine();
+Console.WriteLine($"""
+    //*La cadena {Texto1} con la cadena {Texto2} forman: {Texto1.Concat(Texto2)}
+""");
---
+++ Punto3/readme.md
@@ -0,0 +1,3 @@
+String es un tipo por referenia
+Tiene /0, /n, /"
+cuado usas @ y $ antes de una cadena permitira la escritura en multiples renglones e ingresar parametros dentro del texto de una forma mas ordenada, prolija y bien  entre llaves.
